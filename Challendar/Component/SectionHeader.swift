eaccae5f25522f457b395e8f18760f56d0d5da40
hyelim9300 <hye930011@naver.com>
2024-06-10 11:41:14 +0900

    [ReFactor]#17- 섹션 헤더 컴포넌트 추가

--- Challendar/Component/SectionHeader.swift
+++ Challendar/Component/SectionHeader.swift
@@ -6,31 +6,42 @@
 //
 
 import UIKit
+import SnapKit
 
 class SectionHeader: UICollectionReusableView {
     let sectionLabel: UILabel = {
         let label = UILabel()
-//        label.backgroundColor = .red
         label.font = .pretendardSemiBold(size: 14)
         label.textColor = .challendarBlack60
-        label.translatesAutoresizingMaskIntoConstraints = false
         return label
+    }()
+    
+    let deleteLabel: UIButton = {
+        let button = UIButton()
+        button.setTitle("지우기", for: .normal)
+        button.setTitleColor(.challendarBlack60, for: .normal)
+        button.titleLabel?.font = .pretendardSemiBold(size: 14)
+        button.isHidden = true
+        return button
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(sectionLabel)
-        NSLayoutConstraint.activate([
-            sectionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
-            sectionLabel.topAnchor.constraint(equalTo: topAnchor, constant: 0),
-            sectionLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
-            sectionLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0)
-        ])
+        addSubview(deleteLabel)
+        
+        sectionLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.top.bottom.equalToSuperview()
+        }
+        
+        deleteLabel.snp.makeConstraints { make in
+            make.trailing.equalToSuperview()
+            make.top.bottom.equalToSuperview()
+        }
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
-
-// 리로드 데이터 할 때 diffable, hashable
