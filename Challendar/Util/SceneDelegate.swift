ed5c4c6b2c80759aad001eed0864b680ffcd8005
chynmn <chynmn0523@gmail.com>
2024-06-12 20:59:21 +0900

    fix: sceneDelegate

--- Challendar/Util/SceneDelegate.swift
+++ Challendar/Util/SceneDelegate.swift
@@ -15,13 +15,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let  windowScene = (scene as? UIWindowScene) else {return}
         window = UIWindow(windowScene: windowScene)
-   //     window?.rootViewController = TabBarViewController()
+        window?.rootViewController = TabBarViewController()
         
         
         //네비게이션 임베드
-        let nav = UINavigationController(rootViewController: EditTodoTitleViewController())
-        window?.rootViewController = nav
-        
+//        let nav = UINavigationController(rootViewController: EditTodoTitleViewController())
+//        window?.rootViewController = nav
+//        
         
         window?.makeKeyAndVisible()
     }
