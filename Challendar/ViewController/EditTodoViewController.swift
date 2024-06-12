6f1fa1a37489514f93240bff1e188245ae0c9daf
hyelim9300 <hye930011@naver.com>
2024-06-13 00:35:45 +0900

    [Feat]#37-2- 값 받아오는 로직

--- Challendar/ViewController/EditTodoViewController.swift
+++ Challendar/ViewController/EditTodoViewController.swift
@@ -8,10 +8,7 @@
 import UIKit
 import SnapKit
 
-
-
 class EditTodoTitleViewController: BaseViewController {
-    
     
     // 계획 질문 UI 컴포넌트
     let titleLabel = EditTitleLabel(text: "어떤 계획이 생겼나요?")
@@ -23,54 +20,46 @@ class EditTodoTitleViewController: BaseViewController {
     let dateAskLabel = EditTitleLabel(text: "기한을 선택해주세요")
     let dateAskView = EmptyView()
     let dateView = DateView()
-    var newTodo : Todo?
+    
+    var newTodo: Todo?
+    var todoId: UUID? // 전달받은 투두의 ID를 저장할 변수
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureNavigationBar(checkFirst: true)
-        
-        titleLabel.backgroundColor = .systemPink
-        titleView.backgroundColor = .cyan
-        todoTextField.backgroundColor = .black
-        
-        dateAskLabel.backgroundColor = .blue
-        dateAskView.backgroundColor = .green
+        fetchTodo()
     }
     
-    
-    override func configureConstraint(){
-        
-        
+    override func configureConstraint() {
         titleView.addSubview(todoTextField)
-        dateAskView.addSubview(dateView) // 나연님 짱..
+        dateAskView.addSubview(dateView)
         
-        [titleLabel, titleView, dateAskLabel, dateAskView].forEach{
+        [titleLabel, titleView, dateAskLabel, dateAskView].forEach {
             self.view.addSubview($0)
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
         
-        titleLabel.snp.makeConstraints{
+        titleLabel.snp.makeConstraints {
             $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(titleToNav)
             $0.bottom.equalTo(titleView.snp.top).inset(-titleToCell)
             $0.leading.trailing.equalTo(self.view.safeAreaLayoutGuide).inset(horizontalPadding)
         }
-        titleView.snp.makeConstraints{
+        titleView.snp.makeConstraints {
             $0.height.equalTo(cellHeight)
             $0.leading.trailing.equalTo(self.view.safeAreaLayoutGuide).inset(horizontalPadding)
         }
-        todoTextField.snp.makeConstraints{
+        todoTextField.snp.makeConstraints {
             $0.top.bottom.equalToSuperview().inset(textFieldVerticalPadding)
             $0.leading.trailing.equalToSuperview().inset(textFieldHorizontalPadding)
         }
         
-        dateAskLabel.snp.makeConstraints{
+        dateAskLabel.snp.makeConstraints {
             $0.top.equalTo(titleView.snp.bottom).offset(24)
             $0.leading.equalTo(view.safeAreaLayoutGuide).offset(16)
             $0.trailing.equalTo(view.safeAreaLayoutGuide).offset(-16)
-            
         }
-        dateAskView.snp.makeConstraints{
+        dateAskView.snp.makeConstraints {
             $0.height.equalTo(cellHeight)
             $0.top.equalTo(dateAskLabel.snp.bottom).offset(16)
             $0.leading.trailing.equalTo(self.view.safeAreaLayoutGuide).inset(horizontalPadding)
@@ -81,7 +70,31 @@ class EditTodoTitleViewController: BaseViewController {
             $0.top.bottom.equalToSuperview().inset(dateLabelVerticalPadding)
         }
     }
+    
+    private func fetchTodo() {
+        guard let todoId = todoId, let todoModel = CoreDataManager.shared.fetchTodoById(id: todoId) else {
+            print("Todo not found")
+            return
+        }
+        
+        // 투두의 제목을 텍스트 필드에 설정
+        todoTextField.text = todoModel.title
+        
+        // 투두의 날짜 범위를 dateView에 설정
+        if let startDate = todoModel.startDate, let endDate = todoModel.endDate {
+            if startDate.isSameDay(as: endDate) {
+                dateView.textLabel.text = DateRange.today.rawValue
+            } else if startDate.isSameDay(as: Date().addingDays(-1)!) && endDate.isSameDay(as: Date()) {
+                dateView.textLabel.text = DateRange.tomorrow.rawValue
+            } else if startDate.isSameDay(as: Date().addingDays(-7)!) && endDate.isSameDay(as: Date()) {
+                dateView.textLabel.text = DateRange.week.rawValue
+            } else {
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyy. M. d"
+                let startDateString = dateFormatter.string(from: startDate)
+                let endDateString = dateFormatter.string(from: endDate)
+                dateView.textLabel.text = "\(startDateString) - \(endDateString)"
+            }
+        }
+    }
 }
-
-
-
