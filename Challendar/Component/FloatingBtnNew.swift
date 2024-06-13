3e55b410e161f81851dd5e9d7b7c50cf80831c31
Nayeon Chae <cny1230@naver.com>
2024-06-14 02:25:46 +0900

    FloatingButton edited

---
+++ Challendar/Component/FloatingBtnNew.swift
@@ -0,0 +1,47 @@
+//
+//  FloatingBtnNew.swift
+//  Challendar
+//
+//  Created by 채나연 on 6/14/24.
+//
+
+import UIKit
+
+class FloatingBtnNew: UIButton {
+    
+    
+    //class FloatingButton: UIButton {
+    //
+    //    override init(frame: CGRect) {
+    //        super.init(frame: frame)
+    //        setup()
+    //    }
+    //
+    //    required init?(coder: NSCoder) {
+    //        super.init(coder: coder)
+    //        setup()
+    //    }
+    //
+    //    private func setup() {
+    //        self.backgroundColor = .alertEmerald
+    //        self.setImage(.fab.withTintColor(.challendarBlack, renderingMode: .alwaysOriginal), for: .normal)
+    //        self.layer.cornerRadius = 60 / 2
+    //        self.layer.borderColor = UIColor.secondary600.cgColor
+    //        self.layer.borderWidth = 1
+    //        self.clipsToBounds = true
+    //        setupShadow()
+    //    }
+    //
+    //    private func setupShadow(){
+    //        self.layer.masksToBounds = false
+    //        self.layer.shadowOpacity = 1
+    //        self.layer.shadowRadius = 8
+    //        self.layer.shadowOffset = CGSize(width: 0, height: 4)
+    //        self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.45).cgColor
+    //    }
+    //
+    //    override var intrinsicContentSize: CGSize {
+    //        return CGSize(width: 60, height: 60)
+    //    }
+    //}
+}
--- Challendar/Component/FloatingButton.swift
+++ Challendar/Component/FloatingButton.swift
@@ -8,36 +8,58 @@
 import UIKit
 
 class FloatingButton: UIButton {
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setup()
+        configureUI()
     }
-    
+
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        setup()
+        configureUI()
     }
-    
-    private func setup() {
-        self.backgroundColor = .alertEmerald
-        self.setImage(.fab.withTintColor(.challendarBlack, renderingMode: .alwaysOriginal), for: .normal)
-        self.layer.cornerRadius = 60 / 2
-        self.layer.borderColor = UIColor.secondary600.cgColor
-        self.layer.borderWidth = 1
-        self.clipsToBounds = true
+
+    private func configureUI() {
+        // 기본 구성
+        var config = UIButton.Configuration.filled()
+        config.baseBackgroundColor = .challendarBlack
+        config.baseForegroundColor = .challendarWhite
+        config.cornerStyle = .capsule
+        
+        // 타이틀과 이미지 설정
+        config.title = "할 일 추가"
+        titleLabel?.font = .pretendardRegular(size: 16)
+        titleLabel?.textColor = .challendarWhite
+        config.image = UIImage(systemName: "plus")
+        
+        // 타이틀과 이미지 간격 설정
+        config.imagePadding = 4 // 이미지와 타이틀 간격
+        config.imagePlacement = .leading
+        
+        // 구성 적용
+        self.configuration = config
+        
+        // 그림자 및 보더 설정
         setupShadow()
+        setupBorder()
     }
-    
-    private func setupShadow(){
+
+    private func setupShadow() {
         self.layer.masksToBounds = false
-        self.layer.shadowOpacity = 1
-        self.layer.shadowRadius = 8
-        self.layer.shadowOffset = CGSize(width: 0, height: 4)
-        self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.45).cgColor
+        self.layer.shadowOpacity = 0.5
+        self.layer.shadowRadius = 4
+        self.layer.shadowOffset = CGSize(width: 0, height: 2)
+        self.layer.shadowColor = UIColor.black.cgColor
     }
     
+    private func setupBorder() {
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor.secondary800.cgColor
+        self.layer.cornerRadius = 26
+    }
+
     override var intrinsicContentSize: CGSize {
-        return CGSize(width: 60, height: 60)
+        return CGSize(width: 128, height: 52)
     }
 }
+
