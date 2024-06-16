f138c0c64d3f743cae29e50b1b6746eb26a62f09
hyelim9300 <hye930011@naver.com>
2024-06-16 15:02:09 +0900

    [Fix]#67-새로운 도전, 계획 todo 등록 후 해당 탭 인덱스로 이동

--- Challendar/ViewController/AddTodo/SuccessViewController.swift
+++ Challendar/ViewController/AddTodo/SuccessViewController.swift
@@ -16,21 +16,22 @@ class SuccessViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavigationBarForSuccess()
+        navigateToAppropriateTab()
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
-            self.dismiss(animated: true)
+            self.dismiss(animated: true, completion: nil)
         })
-        
     }
+
     override func configureUI() {
         dimmedView.backgroundColor = UIColor.secondary900.withAlphaComponent(dimmedViewAlpha)
         if isChallenge {
             imageView.image = .partyPopper
             textLabel.text = "도전 목록에 추가했어요!"
-        }else{
+        } else {
             imageView.image = .calendarIcon
             textLabel.text = "계획 목록에 추가했어요!"
         }
@@ -39,24 +40,35 @@ class SuccessViewController: BaseViewController {
         textLabel.backgroundColor = .clear
         imageView.backgroundColor = .clear
     }
+
     override func configureConstraint() {
-        [dimmedView,imageView,textLabel].forEach{
+        [dimmedView, imageView, textLabel].forEach {
             self.view.addSubview($0)
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
-        dimmedView.snp.makeConstraints{
+        dimmedView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
-        imageView.snp.makeConstraints{
+        imageView.snp.makeConstraints {
             $0.size.equalTo(100)
             $0.centerX.equalToSuperview()
             $0.top.equalToSuperview().offset(297)
         }
-        
-        textLabel.snp.makeConstraints{
+        textLabel.snp.makeConstraints {
             $0.top.equalTo(imageView.snp.bottom).offset(32)
             $0.centerX.equalToSuperview()
         }
     }
 
+    private func navigateToAppropriateTab() {
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let window = windowScene.windows.first,
+           let tabBarController = window.rootViewController as? TabBarViewController {
+            if isChallenge {
+                tabBarController.selectedIndex = 0
+            } else {
+                tabBarController.selectedIndex = 2
+            }
+        }
+    }
 }
